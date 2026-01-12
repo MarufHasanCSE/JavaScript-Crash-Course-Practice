de2fdf6d9b9a6e0e02a186ca55d7ee3266fc4dc2
Md Maruf Hasan <160125834+MarufHasanCSE@users.noreply.github.com>
2026-01-12 22:03:36 +0600

    Add files via upload

---
+++ primitiveVsReference.js
@@ -0,0 +1,22 @@
+let  x = 5;
+let y = 6;
+
+x= y;
+y = 7;
+
+console.log(x);
+console.log(y);
+
+
+
+let p = ["Js", "python"];
+let q = ["Html", " CSS"];
+p = q;
+
+console.log(p);
+console.log(q);
+
+p.push("C++");
+
+console.log(p);
+console.log(q);
