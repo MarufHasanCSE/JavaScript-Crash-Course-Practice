247de5685103a6fae0a10db9722dd11c9ac512f8
Md Maruf Hasan <160125834+MarufHasanCSE@users.noreply.github.com>
2026-01-13 16:22:37 +0600

    Add files via upload

---
+++ TernaryOperator.js
@@ -0,0 +1,10 @@
+
+// Ternary Operator
+const a = 6;
+
+const result = a > 5 ? 'Greater than 5' : '5 or less';
+console.log(result); // Output: Greater than 5
+
+const age = 20;
+const canVote = age >= 18 ? 'Yes, can vote' : 'No, cannot vote';
+console.log(canVote); // Output: Yes, can vote
